ed5a8938e76361ad1c99499114fcc10682d97b8b
MinhajLahith2002 <minhajse2023@gmail.com>
2025-12-10 11:55:42 +0530

    Normalize line endings to LF

--- context/TripDetailContext.tsx
+++
@@ -1,9 +0,0 @@
-import { TripInfo } from "@/app/create-new-trip/_components/ChatBox";
-import React, { createContext } from "react";
-
-export type TripContextType={
-    tripDetailInfo:TripInfo | null,
-    setTripDetailInfo:React.Dispatch<React.SetStateAction<TripInfo | null>>
-}
-
-export const TripDetailContext = createContext<TripContextType|undefined>(undefined);
